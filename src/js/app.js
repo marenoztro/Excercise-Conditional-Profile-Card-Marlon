bace679cef0e2b4dd870a463b7bd31bf32f1ef69
marenoztro <marlon.marotto@gmail.com>
2022-08-09 20:02:37 +0000

    Propiedades del Objeto Variables Actualizadas y rev por Diego Bullor

--- src/js/app.js
+++ src/js/app.js
@@ -33,14 +33,28 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1>${variables.name === null ? "Tu Nombre" : variables.name} ${
+    variables.lastname === null ? "Tu Apellido" : variables.lastname
+  }</h1>
+          <h2>${variables.role === null ? "Tu Role" : variables.role}</h2>
+          <h3>${variables.city === null ? "Tu Ciudad" : variables.ciy}, ${
+    variables.country === null ? "Tu País" : variables.country
+  }</h3>
+          <ul class=${variables.socialMediaPosition}>
+            <li><a href="https://twitter.com/${
+              variables.twitter === null ? "Tu Twitter" : variables.twitter
+            }"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${
+              variables.github === null ? "Tu Github" : variables.github
+            }"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/${
+              variables.linkedin === null ? "Tu LinkedIn" : variables.linkedin
+            }"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${
+              variables.instagram === null
+                ? "Tu Instagram"
+                : variables.instagram
+            }"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
